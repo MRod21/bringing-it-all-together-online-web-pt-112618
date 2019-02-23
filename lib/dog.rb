c1ff835ab28400ec16aaa8338275e5a07cf8f7b1
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-23 03:51:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Dog 
-  attr_accessor :name, :breed
+  attr_accessor :name, :breed, :id
   attr_reader :id
   
   def initialize(id: nil, name:, breed:)
