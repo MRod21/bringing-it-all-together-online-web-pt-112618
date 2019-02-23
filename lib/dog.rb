5798781e35a9ed2cea334bf500603ced5c2ee2c9
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-23 03:39:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,6 +25,14 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    
+    self.new(id, name, breed)
+  end
+  
   def save
     if self.id
       self.update
