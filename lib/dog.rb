fda780624b0604c117a88f0519affc06326b15fd
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-23 04:02:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      Dog.new(id: id, name: name, breed: breed)
+      Dog.new(id: nil, name: name, breed: breed)
     end
     dog 
   end
