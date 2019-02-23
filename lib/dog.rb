51cd7658c68560ed93ff03c664dc818ba6fd3c25
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-23 03:48:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,9 @@ class Dog
   
   def update
     sql = "UPDATE dogs SET name = ?, breed = ?, WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
   
   def save
     if self.id
